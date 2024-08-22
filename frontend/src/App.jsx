9eecd52c1b444bd7c2b054ac6dbb5361ed8e4b11
magnificodev <magnificodev8@gmail.com>
2024-08-22 13:23:06 +0700

    create pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+
 const App = () => {
-    return <h1 className="text-3xl text-red-500">Magnificodev</h1>
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/sign-up" element={<SignUp />} />
+                <Route path="/sign-in" element={<SignIn />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/projects" element={<Projects />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
---
+++ frontend/src/pages/SignIn.jsx
@@ -0,0 +1,5 @@
+const SignIn = () => {
+    return <div>This is SignIn Page</div>
+};
+
+export default SignIn;
