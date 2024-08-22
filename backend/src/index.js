1d64ba3e8346a266c89f065dcf6406a0de5b0b73
magnificodev <magnificodev8@gmail.com>
2024-08-22 15:42:02 +0700

    connect to the database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,24 @@
-import express from "express"
-import "dotenv/config"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+// Constant variables
 const PORT = process.env.PORT_NUMBER || 3000;
+
+// Load environment variables from .env file
+dotenv.config();
+
+
+// Connect the database
+mongoose
+    .connect(process.env.MONGODB_CONNECTION_STRING)
+    .then(() => console.log("MongoDB is connected successfully"))
+    .catch((err) => console.log(err));
 
 const app = express();
 
+
+// Listen the upcoming requests
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
