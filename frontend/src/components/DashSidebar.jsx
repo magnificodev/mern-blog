a199de52ee7e2c0f18881ff663a434f773129b76
magnificodev <magnificodev8@gmail.com>
2024-09-16 11:48:11 +0700

    add posts section to the dashboard

--- frontend/src/components/DashSidebar.jsx
+++ frontend/src/components/DashSidebar.jsx
@@ -1,11 +1,21 @@
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { Sidebar } from "flowbite-react";
-import { HiUser, HiArrowSmRight } from "react-icons/hi";
+import {
+    HiUser,
+    HiArrowSmRight,
+    HiDocumentText,
+    HiChartPie,
+    HiAnnotation,
+    HiOutlineUserGroup,
+} from "react-icons/hi";
+import { useSelector } from "react-redux";
 
 const DashSidebar = () => {
     const location = useLocation();
     const [tab, setTab] = useState("");
+
+    const { currentUser } = useSelector((state) => state.user);
 
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search);
@@ -18,16 +28,56 @@ const DashSidebar = () => {
     return (
         <Sidebar className="w-full">
             <Sidebar.Items>
-                <Sidebar.ItemGroup>
+                <Sidebar.ItemGroup className="flex flex-col gap-1">
+                    {currentUser.isAdmin && (
+                        <Sidebar.Item
+                            href="/dashboard/?tab=dashboard"
+                            icon={HiChartPie}
+                            labelColor="dark"
+                            active={tab === "dashboard"}
+                        >
+                            Dashboard
+                        </Sidebar.Item>
+                    )}
                     <Sidebar.Item
                         href="/dashboard/?tab=profile"
                         icon={HiUser}
-                        label="User"
+                        label={currentUser.isAdmin ? "Admin" : "User"}
                         labelColor="dark"
                         active={tab === "profile"}
                     >
                         Profile
                     </Sidebar.Item>
+                    {currentUser.isAdmin && (
+                        <Sidebar.Item
+                            href="/dashboard/?tab=comments"
+                            icon={HiAnnotation}
+                            labelColor="dark"
+                            active={tab === "comments"}
+                        >
+                            Comments
+                        </Sidebar.Item>
+                    )}
+                    {currentUser.isAdmin && (
+                        <Sidebar.Item
+                            href="/dashboard/?tab=users"
+                            icon={HiOutlineUserGroup}
+                            labelColor="dark"
+                            active={tab === "users"}
+                        >
+                            Users
+                        </Sidebar.Item>
+                    )}
+                    {currentUser.isAdmin && (
+                        <Sidebar.Item
+                            href="/dashboard/?tab=posts"
+                            icon={HiDocumentText}
+                            labelColor="dark"
+                            active={tab === "posts"}
+                        >
+                            Posts
+                        </Sidebar.Item>
+                    )}
                     <Sidebar.Item href="#" icon={HiArrowSmRight}>
                         Sign out
                     </Sidebar.Item>
--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -83,7 +83,7 @@ const CreatePost = () => {
     return (
         <div className="p-3 max-w-3xl mx-auto min-h-screen">
             <h1 className="text-center text-3xl my-7 font-semibold">
-                Create Post
+                Create a Post
             </h1>
             <form
                 onSubmit={handleSubmit(onSubmit)}
