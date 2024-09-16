6820155ca9f373143d878e90d628fe2a978bd630
magnificodev <magnificodev8@gmail.com>
2024-09-16 12:40:30 +0700

    create get posts api route

--- backend/src/routes/posts.route.js
+++ backend/src/routes/posts.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import verifyToken from "../middlewares/auth.middleware.js";
-import { createPost } from "../controllers/posts.controller.js";
+import { createPost, getPosts } from "../controllers/posts.controller.js";
 import { createValidator } from "../validators/post.validator.js";
 
 const router = express.Router();
 
 router.post("/", verifyToken, createValidator, createPost);
+router.get("/", getPosts)
 
 export default router;
